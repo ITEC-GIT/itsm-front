3e3b2802ec33d8ac8826ee671542bb2a02c93dac
zoughaib-sally <s.zoughaib@itec.co.ao>
2025-03-06 14:02:30 +0200

    adjust search input

--- src/app/components/form/search.tsx
+++ src/app/components/form/search.tsx
@@ -8,17 +8,12 @@ const SearchComponent = ({
   return (
     <div style={{ width: "100%", maxWidth: "300px" }}>
       <div className="input-group">
-        <span className="input-group-text custom-input" id="search-icon">
-          <i className="bi bi-search"></i>
-        </span>
         <input
           type="text"
-          className="form-control custom-input"
+          className="form-control form-control-solid"
           placeholder="Search..."
           value={value}
           onChange={onChange}
-          aria-label="Search"
-          aria-describedby="search-icon"
         />
       </div>
     </div>
