4e4514245c191304814a7c441507637595da9c5f
zoughaib-sally <s.zoughaib@itec.co.ao>
2025-03-12 11:37:06 +0200

    assets page modification

--- src/app/pages/Assets-page/assetsPage.tsx
+++ src/app/pages/Assets-page/assetsPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { SearchComponent } from "../../components/form/search";
-import DataTable, { SortOrder, TableColumn } from "react-data-table-component";
+import DataTable, { TableColumn } from "react-data-table-component";
 import { useAtom } from "jotai";
 import { sidebarToggleAtom } from "../../atoms/sidebar-atom/sidebar";
 import {
@@ -20,6 +20,10 @@ import AnimatedRouteWrapper from "../../routing/AnimatedRouteWrapper.tsx";
 
 const AssetsPage = () => {
   const tableContainerRef = useRef<HTMLDivElement>(null);
+
+  const divRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+
   const [currentHistorysPage, setCurrentHistoryPage] = useState<number>(1);
   const [ShowActionColumn, setShowActionColumn] = useAtom(showActionColumnAtom);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -208,24 +212,29 @@ const AssetsPage = () => {
     () => memoizedColumns.filter((col) => col.id !== "action"),
     [memoizedColumns]
   );
+
+  useEffect(() => {
+    if (divRef.current) {
+      const rect = divRef.current.getBoundingClientRect();
+      setHeight(Math.round(rect.height));
+    }
+  }, [divRef.current]);
+
   return (
     <AnimatedRouteWrapper>
-      <div className="card-container custom-container-height">
-        <div className="d-flex flex-column custom-main-container height-100">
-          <div className="container-fluid d-flex flex-column mt-4 height-100">
-            <div
-              className="content-container d-flex flex-column"
-              style={{
-                marginRight: isSidebarOpen ? "15%" : "0",
-                width: isSidebarOpen ? "78%" : "100%",
-                overflow: "hidden",
-              }}
-            >
+      <div className="card-container h-100 d-flex flex-column pt-3 pb-3">
+        <div className="row d-flex flex-column custom-main-container custom-container-height">
+          <div
+            className={`d-flex flex-column ${
+              isSidebarOpen ? "col-9" : "col-12"
+            }`}
+          >
+            <div ref={divRef}>
               <div className="d-flex mb-3 ms-2">
-                <h2 className="mb-4">🛠️ Assets</h2>
+                <h2>🛠️ Assets</h2>
               </div>
 
-              <div className="row justify-content-around p-3">
+              <div className="row justify-content-between p-3">
                 <div className="col-sm-12 col-md-6 d-flex align-items-center gap-2">
                   <button className="btn custom-btn">
                     <i className="bi bi-cloud-download text-dark custom-btn-icon"></i>
@@ -272,7 +281,14 @@ const AssetsPage = () => {
                   </button>
                 </div>
               </div>
+            </div>
 
+            <div
+              style={{
+                height: `calc(100vh - var(--bs-app-header-height) - 40px - ${height}px - 40px)`,
+                overflow: "auto",
+              }}
+            >
               <div
                 className="p-3"
                 ref={tableContainerRef}
@@ -296,67 +312,84 @@ const AssetsPage = () => {
                   onRowMouseLeave={handleMouseLeave}
                 />
               </div>
-
-              <div className="tickets-pagination-controls">
-                <button
-                  className="btn btn-sm btn-light me-2"
-                  onClick={handleFirstPage}
-                  disabled={currentHistorysPage === 1}
-                >
-                  First
-                </button>
-                <button
-                  className="btn btn-sm btn-light me-2"
-                  onClick={handlePreviousPage}
-                  disabled={currentHistorysPage === 1}
-                >
-                  Previous
-                </button>
-                {Array.from(
-                  { length: endPage - startPage + 1 },
-                  (_, index) => startPage + index
-                ).map((page) => (
-                  <button
-                    key={page}
-                    className={clsx("btn btn-sm me-2", {
-                      "btn-primary": currentHistorysPage === page,
-                      "btn-light": currentHistorysPage !== page,
-                    })}
-                    onClick={() => handlePageChange(page)}
-                  >
-                    {page}
-                  </button>
-                ))}
-                <button
-                  className="btn btn-sm btn-light me-2"
-                  onClick={handleNextPage}
-                  disabled={currentHistorysPage === totalPages}
-                >
-                  Next
-                </button>
-                <button
-                  className="btn btn-sm btn-light"
-                  onClick={handleLastPage}
-                  disabled={currentHistorysPage === totalPages}
-                >
-                  Last
-                </button>
-              </div>
             </div>
 
             <div
-              className={`sidebar-container ${
-                isSidebarOpen ? "sidebar-open" : "sidebar-closed"
-              }`}
+              className="d-flex justify-content-end align-items-center"
+              style={{
+                position: "sticky",
+                bottom: 0,
+                height: "40px",
+                // top: "100%",
+                // right: "0",
+                width: "100%",
+                zIndex: "2000",
+              }}
             >
-              <FilterSidebar
-                isOpen={isSidebarOpen}
-                toggleSidebar={toggleSidebar}
-                activeFilters={activeFilters}
-                saveFilters={setFilters}
-              />
+              <button
+                className="btn btn-sm btn-light me-2"
+                onClick={handleFirstPage}
+                disabled={currentHistorysPage === 1}
+              >
+                First
+              </button>
+              <button
+                className="btn btn-sm btn-light me-2"
+                onClick={handlePreviousPage}
+                disabled={currentHistorysPage === 1}
+              >
+                Previous
+              </button>
+              {Array.from(
+                { length: endPage - startPage + 1 },
+                (_, index) => startPage + index
+              ).map((page) => (
+                <button
+                  key={page}
+                  className={clsx("btn btn-sm me-2", {
+                    "btn-primary": currentHistorysPage === page,
+                    "btn-light": currentHistorysPage !== page,
+                  })}
+                  onClick={() => handlePageChange(page)}
+                >
+                  {page}
+                </button>
+              ))}
+              <button
+                className="btn btn-sm btn-light me-2"
+                onClick={handleNextPage}
+                disabled={currentHistorysPage === totalPages}
+              >
+                Next
+              </button>
+              <button
+                className="btn btn-sm btn-light"
+                onClick={handleLastPage}
+                disabled={currentHistorysPage === totalPages}
+              >
+                Last
+              </button>
             </div>
           </div>
+
+          {isSidebarOpen && (
+            <div
+              className="col-3 custom-border p-0"
+              style={{
+                height: `calc(100vh - var(--bs-app-header-height) - 40px)`,
+                overflow: "auto",
+              }}
+            >
+              <div>
+                <FilterSidebar
+                  isOpen={isSidebarOpen}
+                  toggleSidebar={toggleSidebar}
+                  activeFilters={activeFilters}
+                  saveFilters={setFilters}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </AnimatedRouteWrapper>
