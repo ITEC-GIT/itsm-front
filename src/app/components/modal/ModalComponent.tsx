40a125568aeb078918845d6350b222e8183fd44f
zoughaib-sally <s.zoughaib@itec.co.ao>
2025-05-08 10:01:44 +0300

    modify custom modal

--- src/app/components/modal/ModalComponent.tsx
+++ src/app/components/modal/ModalComponent.tsx
@@ -5,6 +5,7 @@ interface ModalComponentProps {
   onConfirm: () => void;
   onCancel: () => void;
   message: string;
+  type: "error" | "success" | "warning";
 }
 
 const ModalComponent = ({
@@ -12,36 +13,51 @@ const ModalComponent = ({
   onConfirm,
   onCancel,
   message,
+  type,
 }: ModalComponentProps) => {
   if (!isOpen) {
     return null;
   }
 
+  const typeIconClass: Record<ModalComponentProps["type"], string> = {
+    error: "fas fa-times-circle text-danger",
+    success: "fas fa-check-circle text-success",
+    warning: "fas fa-exclamation-triangle text-warning",
+  };
+
+  const typeBtnClass: Record<ModalComponentProps["type"], string> = {
+    error: "bg-danger",
+    success: "bg-success",
+    warning: "bg-warning",
+  };
+  const iconClass = typeIconClass[type];
+  const btnClass = typeBtnClass[type];
   return (
     <div
       className="modal fade show"
       tabIndex={-1}
       style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+      onClick={onCancel}
     >
-      <div className="modal-dialog modal-dialog-centered">
+      <div
+        className="modal-dialog modal-dialog-centered"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="modal-content">
           <div className="modal-body d-flex align-items-center">
-            <i className="fas fa-exclamation-triangle animated-icon"></i>
+            <i className={`animated-icon ${iconClass}`}></i>
             <div
               className="text-start"
               dangerouslySetInnerHTML={{ __html: message }}
             ></div>
           </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-light" onClick={onCancel}>
-              Cancel
-            </button>
             <button
               type="button"
-              className="btn btn-danger"
+              className={`btn ${btnClass} text-white`}
               onClick={onConfirm}
             >
-              Confirm
+              OK
             </button>
           </div>
         </div>
